f11e7443427080bfe47e53bc4f3b5ea1974de233
Hkmori15 <animeshnik27472@gmail.com>
2024-06-06 23:17:21 +0200

    Bot added

---
+++ index.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const { Bot, GrammyError, HttpError } = require('grammy');
+const bot = new Bot(process.env.BOT_API_KEY);
+
+bot.command('start', async (ctx) => {
+    await ctx.reply('Привет, это тестовый запуск бота')
+})
+
+bot.on('message', async (ctx) => {
+    await ctx.reply('Сообщение получено!')
+});
+
+bot.catch((err) => {
+    const ctx = err.ctx;
+    console.error(`Error while handling an update ${ctx.update_id}:`);
+
+
+    if (e instanceof GrammyError) {
+        console.error('Error in request:', e.description);
+    } else if (e instanceof HttpError) {
+        console.error('Could not contact Telegram', e);
+    } else {
+        console.error('Unknown error', e);
+    }
+});
+bot.start();
